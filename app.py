9dbe82642bf42884775a7069c1d55c4a204e9609
HTYS <1806003183@qq.com>
2026-02-12 16:51:24 +0800

    第一次框架

--- app.py
+++ app.py
@@ -1,3 +1,32 @@
 import random
 import requests
 import json
+
+# 创建用户类
+class User(object):
+
+    def __init__(self, username, post_frequency, post_prompt):
+        self.username = username
+        self.frequency = post_frequency
+        self.prompt = post_prompt
+
+    def post(self):
+        return self.prompt
+
+
+
+# 导入用户信息
+user_config = []
+with open("ai_users_config.json", "r", encoding= "UTF-8") as USER_CONFIG:
+    config = json.load(USER_CONFIG)
+    for user in config["ai_users"]:
+        user_config.append(User(
+            user["username"],
+            user["post_frequency"],
+            user["post_prompt"])
+        )
+
+
+
+
+
