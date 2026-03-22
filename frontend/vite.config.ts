56d6d8424da43b86c75681bbbc99d5df7d697088
HTYS <1806003183@qq.com>
2026-03-22 15:20:44 +0800

    Alpha-v1.7.1-fix: 修复vite.config.ts的路径别名配置重复和错误，tsconfig.json的重复子路径别名

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,11 +8,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@/app': path.resolve(__dirname, './src/app'),
-      '@/features': path.resolve(__dirname, './src/features'),
-      '@/pages': path.resolve(__dirname, './src/pages'),
-      '@/widgets': path.resolve(__dirname, './src/widgets'),
-      '@/shared': path.resolve(__dirname, './src/shared'),
     },
   },
   server: {
