26a9dff2bb3cb123d426c2197e5b160c2f327373
HTYS <1806003183@qq.com>
2026-04-11 17:09:18 +0800

    fix: 修复错误的注释

--- agent_scheduler/__init__.py
+++ agent_scheduler/__init__.py
@@ -3,7 +3,6 @@
 #
 # 此文件的作用：
 # 1. 将 agent_scheduler 目录标记为一个 Python 包
-# 2. 通过 pyproject.toml 的 setuptools 配置，可以被 pip install -e . 安装
-# 3. 使得 from agent_scheduler.xxx 可以正常工作
+# 2. 通过 PYTHONPATH=/app 和绝对导入，使得 from agent_scheduler.xxx 可以正常工作
 
 __pkg_name__ = "agent_scheduler"
